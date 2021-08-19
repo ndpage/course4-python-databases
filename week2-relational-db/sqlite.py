459a38da8a12a1cb8aa263a3a33369a030a2c850
Nathan Page <33336363+ndpage@users.noreply.github.com>
2021-08-19 20:50:19 +0000

    Test file to create and connect to database in Python

--- week2-relational-db/sqlite.py
+++ week2-relational-db/sqlite.py
@@ -1,2 +1,11 @@
-import sqlite
+import sqlite3
 
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor() # Cursor opens a record (similar to file handle)
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+ CREATE TABLE Counts (email TEXT, count INTEGER)
+'''
+)
